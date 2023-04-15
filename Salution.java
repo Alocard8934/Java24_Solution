b5daa623c70a2310ef71b8a12f56c3359b332b40
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2023-04-15 21:00:49 +0200

    Add files via upload
    First lesson. The created program displays the inscription "Hello world" in different languages. To change the language, you need to change the call to the variable.

---
+++ Salution.java
@@ -0,0 +1,12 @@
+package JAVA24;
+
+public class Salution {
+     public static void main(String[] args) {
+        String greeting = "Saluton mondo!";
+        String deutsch = "Hallo Welt!!!";
+        String france = "Bonjour le monde!!!";
+        String italy = "Ciao mondo!!!";
+        String portu = "Olá Mundo!!!";
+        System.out.println(portu);
+    }
+}
